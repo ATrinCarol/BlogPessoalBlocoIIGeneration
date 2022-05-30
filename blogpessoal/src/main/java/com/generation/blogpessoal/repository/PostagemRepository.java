26e25d1de893efca6e2c7b1e3b92a8280400809f
Carol Trindade <caroline.alvestrindade@hotmail.com>
2022-05-30 15:30:34 -0300

    Get, Post, Put e Delete

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository; //"JpaRepository" 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository; //"Repository" anotação
 
 import com.generation.blogpessoal.model.Postagem; //"Postagem, Long" Tá pegando o "id" ? da classe Postagem ?
@@ -8,5 +11,8 @@ import com.generation.blogpessoal.model.Postagem; //"Postagem, Long" Tá pegando
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
-
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String Titulo);
+	
+//	public abstract <Postagem> void deletePostagem204 (@Param ("id") Long Id);
+	
 }
